ac58913449cda3a6a1096aa6941a2d8228aa5d43
Jesse <jhuard@ualberta.ca>
2013-10-29 22:22:24 -0600

    Renamed EditFragment to EditFragmentActivity. Added EditFragment, and PreviewFragment Fragments. Load text from Fragment in InfoFragment.

--- CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/InfoFragment.java
+++ CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/InfoFragment.java
@@ -4,9 +4,11 @@
 package cmput301.f13t01.createyourownadventure;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * @author Jesse Huard
@@ -17,6 +19,19 @@ public class InfoFragment extends android.support.v4.app.Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for fragment information
-		return inflater.inflate(R.layout.fragment_info, container, false);
+		View infoView = inflater.inflate(R.layout.fragment_info, container, false);
+		
+		// Get the StoryFragment from the arguments
+		Fragment fragment = (Fragment) getArguments().getSerializable(getResources().getString(R.string.story_fragment));
+		
+		// Find the proper EditText views
+		EditText title = (EditText) infoView.findViewById(R.id.fragment_title);
+		EditText description = (EditText) infoView.findViewById(R.id.fragment_description);
+		
+		// Initialize the views from the StoryFragment
+		title.setText(fragment.getTitle());
+		description.setText(fragment.getDescription());
+		
+		return infoView;
 	}
 }
