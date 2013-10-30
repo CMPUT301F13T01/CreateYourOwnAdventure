5ae49db65eb220d63467ad85e3470ee103f210a4
ihateelvis <rmiller3@ualberta.ca>
2013-10-30 12:16:36 -0600

    Adding gitignore and some tests to branch

---
+++ CreateYourOwnAdventure/src/testSrc/testHistory.java
@@ -0,0 +1,26 @@
+package testSrc;
+
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cmput301.f13t01.createyourownadventure.History;
+
+public class testHistory extends TestCase {
+
+	History history;
+	
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+		this.history = new History();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
