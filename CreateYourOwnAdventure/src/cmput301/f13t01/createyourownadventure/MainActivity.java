9f7dc005d127d448b2bfda3a5e26af59fe792422
manweile <manweile@ualberta.ca>
2013-11-02 14:55:27 -0600

    listview and context menu active now

--- CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/MainActivity.java
+++ CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/MainActivity.java
@@ -16,35 +16,60 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */ 
+
+
 package cmput301.f13t01.createyourownadventure;
+
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 /**
  * sets up and handles main screen ui
+ * from the ma
  */
 public class MainActivity extends Activity{
-	//instantiate and initialize click listener and set list view listener
+	//declare and null init story listview, its adapter and listeners
+	private ListView lsvStories = null;
+	private ArrayAdapter<String> objStoryAdapter = null;
 	private OnItemClickListener objListListen = null;
-	//Listview to show locally cached stories
-	private ListView lsvStories;
+	private OnItemLongClickListener objLongListListen = null;
+	
+	//interim array for testing	
+	private String[] storyList = {"Author 1 Story 1", "Author 1 Story 2", "Author 2 Story 1", "Author 2 Story 2"};
 	
 	/**
 	 * create screen
 	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		//get last instance state and set view to main screen
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
-		//set list view widget and its listener
-		this.lsvStories = (ListView) findViewById(R.id.activity_main_story_list);
-		lsvStories.setOnItemClickListener(this.objListListen);
+		//set list view and array adapter
+		lsvStories = (ListView) findViewById(R.id.main_activity_listview);
+		objStoryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, storyList);
+		lsvStories.setAdapter(objStoryAdapter);
+		//register view for the on long click context menu
+		registerForContextMenu(lsvStories);
+		//TODO register on click contextual menu for read story options		
+		
+		
+		//set on click listener and long click listener
+		lsvStories.setOnItemClickListener(objListListen);
+		lsvStories.setOnItemLongClickListener(objLongListListen);
+		
 	}
 	
 	/**
@@ -63,7 +88,7 @@ public class MainActivity extends Activity{
 	{
 	    MenuInflater inflater = getMenuInflater();
 	    //inflate menu items specified in xml and return 
-	    inflater.inflate(R.menu.main_menu, menu);
+	    inflater.inflate(R.menu.main_actionbar_menu, menu);
 	    return super.onCreateOptionsMenu(menu);
 	}
 	
@@ -87,6 +112,40 @@ public class MainActivity extends Activity{
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
+	
+	/**
+	 * create context menu for on long click of story item in story listview
+	 */
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo){
+		super.onCreateContextMenu(menu, v, menuInfo);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_editstory_menu, menu);
+	}
+	
+	/**
+	 * handle user selection of an on long click contextual menu item
+	 */
+	@Override
+	public boolean onContextItemSelected (MenuItem item) {
+		//get the menu item info
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+	    switch (item.getItemId()) {
+        //user wants to edit the story
+        case R.id.action_edit_story:
+        	//
+            startEditStory();
+            return true;
+        //user wants to delete story
+        case R.id.action_delete_story:
+        	//
+            startDeleteStory();
+            return true;	        	
+        default:
+            return super.onOptionsItemSelected(item);
+	    }		
+
+	}
 
 	/**
 	 * starts create new story child activity
@@ -103,4 +162,37 @@ public class MainActivity extends Activity{
 		//quick and dirty test
 		System.out.println("You selected Browse Online Stories");
 	}
+	
+	/**
+	 * starts edit story child activity
+	 */
+	private void startEditStory() {
+		//quick and dirty test
+		System.out.println("You selected Edit Story");
+	}
+
+	/**
+	 * starts browse online story child activity
+	 */
+	private void startDeleteStory() {
+		//quick and dirty test
+		System.out.println("You selected Delete Story");
+	}
+	/**
+	 * starts read from last history child activity
+	 */
+	private void startContinueStory() {
+		//quick and dirty test
+		System.out.println("You selected Continue Story");
+	}
+
+	/**
+	 * starts reading story at beginning child activity
+	 */
+	private void startBeginingStory() {
+		//quick and dirty test
+		System.out.println("You selected Start at Beginning");
+	}
+	
+	
 }
