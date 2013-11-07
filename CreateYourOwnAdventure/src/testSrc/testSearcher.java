486b08c1c11b0754bdd387222157dc18ab38fe5e
Herp Derp <jhchu@ualberta.ca>
2013-11-07 10:51:26 -0700

    My jUnit tests.

---
+++ CreateYourOwnAdventure/src/testSrc/testSearcher.java
@@ -0,0 +1,41 @@
+package testSrc;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cmput301.f13t01.createyourownadventure.Searcher;
+
+public class testSearcher extends TestCase{
+
+	private Searcher searcher;
+	
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		this.searcher = new Searcher();
+	}
+	
+	@Test
+	public void testSearchTitle() {
+		this.searcher.searchTitle();
+		assertTrue(false);
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSearchAuthor() {
+		this.searcher.searchAuthor();
+		assertTrue(false);
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchDescription() {
+		this.searcher.searchDescription();
+		assertTrue(false);
+		fail("Not yet implemented");
+	}
+
+}
