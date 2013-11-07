37a34794864a49bec7d9b9f1833799c848d7a1ab
Jesse <jhuard@ualberta.ca>
2013-11-07 10:37:24 -0700

    Make InfoFragment do some null value checking

--- CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/InfoFragment.java
+++ CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/InfoFragment.java
@@ -5,6 +5,7 @@ package cmput301.f13t01.createyourownadventure;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,19 +20,27 @@ public class InfoFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for fragment information
-		View infoView = inflater.inflate(R.layout.fragment_info, container, false);
-		
+		View infoView = inflater.inflate(R.layout.fragment_info, container,
+				false);
+
 		// Get the StoryFragment from the arguments
-		StoryFragment fragment = (StoryFragment) getArguments().getSerializable(getResources().getString(R.string.story_fragment));
-		
-		// Find the proper EditText views
-		EditText title = (EditText) infoView.findViewById(R.id.fragment_title);
-		EditText description = (EditText) infoView.findViewById(R.id.fragment_description);
-		
-		// Initialize the views from the StoryFragment
-		title.setText(fragment.getTitle());
-		description.setText(fragment.getDescription());
-		
+		Bundle bundle = getArguments();
+		if (bundle != null) {
+			StoryFragment fragment = (StoryFragment) bundle
+					.getSerializable(getResources().getString(
+							R.string.story_fragment));
+
+			// Find the proper EditText views
+			EditText title = (EditText) infoView
+					.findViewById(R.id.fragment_title);
+			EditText description = (EditText) infoView
+					.findViewById(R.id.fragment_description);
+
+			// Initialize the views from the StoryFragment
+			title.setText(fragment.getTitle());
+			description.setText(fragment.getDescription());
+		}
+
 		return infoView;
 	}
 }
