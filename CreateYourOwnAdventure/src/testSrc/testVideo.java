54e6c13d82c5b80a532d52111e38f051692ed379
Herp Derp <jhchu@ualberta.ca>
2013-11-08 04:19:55 -0700

    Small media test skeletons

---
+++ CreateYourOwnAdventure/src/testSrc/testVideo.java
@@ -0,0 +1,21 @@
+package testSrc;
+
+import static org.junit.Assert.fail;
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class testVideo extends TestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	@Test
+	public void testContent() {
+		assertTrue(true);
+		fail("Not yet implemented");
+	}
+}
