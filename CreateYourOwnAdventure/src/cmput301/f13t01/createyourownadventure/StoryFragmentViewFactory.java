3b5291bd69ee420ad38c9c0063a0bc1d0f9d6656
Herp Derp <jhchu@ualberta.ca>
2013-11-30 15:25:45 -0700

    All files have license now

--- CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/StoryFragmentViewFactory.java
+++ CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/StoryFragmentViewFactory.java
@@ -1,3 +1,27 @@
+/*
+The StoryFragmentViewFactory class is used to generate the views for media within
+a fragment.
+
+     Copyright  �2013 Jesse Huard
+    <Contact: jhuard@ualberta.ca>
+    
+    License GPLv3: GNU GPL Version 3
+    <http://gnu.org/licenses/gpl.html>.
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package cmput301.f13t01.createyourownadventure;
 
 import java.io.File;
@@ -18,6 +42,16 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+/**
+ * The StoryFragmentViewFactory class is used to generate the views for media within
+ * a fragment.
+ * 
+ * @author Jesse Huard
+ * @version 1.0, 29/10/13
+ * @see android.app.Application
+ * 
+ */
 
 public class StoryFragmentViewFactory {
 	private static final int horizontalPadding = cmput301.f13t01.createyourownadventure.R.dimen.activity_horizontal_margin;
