a40f9e35422a8ed6103d70bb3ac745d7a6cda5c8
Herp Derp <jhchu@ualberta.ca>
2013-11-30 11:59:51 -0700

    Documented ChoiceListAdapter, added License

--- CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/ChoiceListAdapter.java
+++ CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/ChoiceListAdapter.java
@@ -1,3 +1,29 @@
+/*
+The ChoiceListAdapter class is the ArrayAdapter for the ChoiceList Fragment.
+It handles inflating the list's views by extracting the StoryFragment titles
+from the EditManager based on the StoryFragmentIds contained in the passed
+Choice. Activities using this fragment must implement the ChoiceListListener
+interface.
+
+     Copyright  �2013 Jesse Huard
+    <Contact: jhuard@ualberta.ca>
+    
+    License GPLv3: GNU GPL Version 3
+    <http://gnu.org/licenses/gpl.html>.
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package cmput301.f13t01.createyourownadventure;
 
 import java.util.ArrayList;
@@ -30,6 +56,12 @@ public class ChoiceListAdapter extends ArrayAdapter<Choice> {
 	private final Context context;
 	private final ArrayList<Choice> choices;
 
+	/**
+	 * Constructor, sets the context and choices
+	 * 
+	 * @param context
+	 * @param choices
+	 */
 	public ChoiceListAdapter(Context context, ArrayList<Choice> choices) {
 		super(context, R.layout.choice_list_item, choices);
 		this.context = context;
@@ -37,6 +69,14 @@ public class ChoiceListAdapter extends ArrayAdapter<Choice> {
 	}
 
 	@Override
+	/**
+	 * Creates and inflates views for choices
+	 * 
+	 * @param position Position of the choice to display
+	 * @param convertView View to convert to
+	 * @param parent the parent View
+	 * @return the view of the row
+	 */
 	public View getView(int position, View convertView, ViewGroup parent) {
 		LayoutInflater inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -70,8 +110,7 @@ public class ChoiceListAdapter extends ArrayAdapter<Choice> {
 	/**
 	 * Gets the Choice at the selected position.
 	 * 
-	 * @param position
-	 *            the position of the desired Choice.
+	 * @param position the position of the desired Choice.
 	 * @return the selected Choice
 	 */
 	public Choice getChoiceAtPosition(int position) {
