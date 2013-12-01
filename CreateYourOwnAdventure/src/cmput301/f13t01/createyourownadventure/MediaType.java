d4895333fd6eaed1402e60f8d86ef1bb2b578649
Jesse <jhuard@ualberta.ca>
2013-12-01 13:01:44 -0700

    Added BASE type to MediaType.

--- CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/MediaType.java
+++ CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/MediaType.java
@@ -34,7 +34,7 @@ package cmput301.f13t01.createyourownadventure;
  */
 
 public enum MediaType {
-	TEXT, IMAGE, SOUND, VIDEO, IMAGEURI;
+	TEXT, IMAGE, SOUND, VIDEO, IMAGEURI, BASE;
 
 	@Override
 	public String toString() {
