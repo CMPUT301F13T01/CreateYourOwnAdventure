c9afe0895daf5c8c2a2a887a1f5b50f2254af929
Herp Derp <jhchu@ualberta.ca>
2013-12-01 12:27:25 -0700

    Missing a license

--- CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/Media.java
+++ CreateYourOwnAdventure/src/cmput301/f13t01/createyourownadventure/Media.java
@@ -1,3 +1,24 @@
+/*
+ * The Media<T> interface is an interface which must be implemented by any
+ * classes that will be used as media types inside a <code>Fragment</code>. The
+ * interface takes a generic type argument which defines the type of content
+ * stored inside the media type.
+ * 
+ * Copyright 2013 Gerald Manweiler Eddie Tai Jesse Chu Jesse Huard Reggie Miller
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package cmput301.f13t01.createyourownadventure;
 
 /**
